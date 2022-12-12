3f858a62492a8c6a26783028a45f5e43242ab2b5
CodeDev1980 <98498887+CodeDev1980@users.noreply.github.com>
2022-12-11 19:04:24 -0600

    Added sort method

--- controllers/blogsPage.js
+++ controllers/blogsPage.js
@@ -1,7 +1,7 @@
 const BlogPost = require('../models/BlogPost');
 
 module.exports = async (req, res) => {
-    const articles = await BlogPost.find({}).populate('userid');
+    const articles = await BlogPost.find({}).sort({_id: -1}).populate('userid');
     res.render('blogs', {
         articles
     })
